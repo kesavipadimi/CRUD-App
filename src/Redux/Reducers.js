e6469f0f8291a81f12c31a1fc211526de105b95f
kesavipadimi <kesavi.padimi@gmail.com>
2021-05-01 17:28:28 +0530

    Updates

--- src/Redux/Reducers.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import profileReducer from '../Redux/Profiles/profileReducer';
-
-const rootreducer = combineReducers({
-    profile:profileReducer,
-})
-
-export default rootreducer;
--- src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-import {createStore, compose, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import rootreducer from '../Redux/Reducers';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootreducer, {}, composeEnhancers(applyMiddleware(thunk)))
-
-export default store;
